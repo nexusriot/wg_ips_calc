e70d013bb873fd2a1e83aed99961df2acb6bc1f0
Vladislav Ananev <nexus.riot@gmail.com>
2025-12-04 01:14:12 +0400

    add couple of tests

--- test_wg_ips_core.py
+++ test_wg_ips_core.py
@@ -6,6 +6,7 @@ Run with:
 """
 import unittest
 import ipaddress
+import re
 
 import wg_ips_core as core
 
@@ -48,6 +49,10 @@ class TestCalculateAllowedIps(unittest.TestCase):
         with self.assertRaises(ValueError):
             core.calculate_allowed_ips("", "")
 
+    def test_omit_empty_dis(self):
+         result = core.calculate_allowed_ips("0.0.0.0/0", "")
+         self.assertEqual(result, "AllowedIPs = 0.0.0.0/0")
+
     def test_example_from_readme(self):
         allowed = "0.0.0.0/0, ::/0"
         disallowed = "37.27.12.178, 10.74.0.3/32, 10.74.0.1"
@@ -79,6 +84,51 @@ class TestCalculateAllowedIps(unittest.TestCase):
 
         for n in parsed_nets:
             self.assertNotIn(dis_ip, n)
+
+    @staticmethod
+    def ip_not_in_allowed(ip: str, result_str: str) -> bool:
+        ip_obj = ipaddress.ip_address(ip)
+        # extract networks
+        cidrs = re.findall(r"\d+\.\d+\.\d+\.\d+/\d+", result_str)
+        networks = [ipaddress.ip_network(c) for c in cidrs]
+        return not any(ip_obj in net for net in networks)
+
+    def test_exclude_cloak_external_ip(self):
+        allowed = "0.0.0.0/0"
+        disallowed = "37.27.21.100"
+
+        result = core.calculate_allowed_ips(allowed, disallowed)
+        self.assertTrue(result.startswith("AllowedIPs = 0.0.0.0/3, 32.0.0.0/6, 36.0.0.0/8, 37.0.0.0/12"))
+        self.assertIn("37.16.0.0/13", result)
+        self.assertIn("37.24.0.0/15", result)
+        self.assertIn("37.26.0.0/16", result)
+        self.assertIn("37.27.0.0/20", result)
+        self.assertIn("37.27.16.0/22", result)
+        self.assertIn("37.27.20.0/24", result)
+        self.assertIn("37.27.21.0/26", result)
+        self.assertIn("37.27.21.64/27", result)
+        self.assertIn("37.27.21.96/30", result)
+        self.assertIn("37.27.21.101/32", result)
+        self.assertIn("37.27.21.102/31", result)
+        self.assertIn("37.27.21.104/29", result)
+        self.assertIn("37.27.21.112/28", result)
+        self.assertIn("37.27.21.128/25", result)
+        self.assertIn("37.27.22.0/23", result)
+        self.assertIn("37.27.24.0/21", result)
+        self.assertIn("37.27.32.0/19", result)
+        self.assertIn("37.27.64.0/18", result)
+        self.assertIn("37.27.128.0/17", result)
+        self.assertIn("37.28.0.0/14", result)
+        self.assertIn("37.32.0.0/11", result)
+        self.assertIn("37.64.0.0/10", result)
+        self.assertIn("37.128.0.0/9", result)
+        self.assertIn("38.0.0.0/7", result)
+        self.assertIn("40.0.0.0/5", result)
+        self.assertIn("48.0.0.0/4", result)
+        self.assertIn("64.0.0.0/2", result)
+        self.assertIn("128.0.0.0/1", result)
+
+        self.assertTrue(self.ip_not_in_allowed(disallowed, result))
 
 
 if __name__ == "__main__":
